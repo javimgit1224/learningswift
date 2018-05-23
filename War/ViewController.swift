15650c65b2da4f7f176be86edd60ecafdf82c835
javimgit1224 <36682043+javimgit1224@users.noreply.github.com>
2018-05-23 15:52:54 -0700

    Add files via upload
    added background to the app

---
+++ War/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  War
+//
+//  Created by Javier Melendrez on 5/23/18.
+//  Copyright © 2018 Javier Melendrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
