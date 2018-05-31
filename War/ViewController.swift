473984943164f299aec46aaad6c60ccd8ae8d2a5
javimgit1224 <36682043+javimgit1224@users.noreply.github.com>
2018-05-31 12:16:33 -0700

    Add files via upload
    added iboutlets to the scores and cards
    added inactions to the deal button

--- War/ViewController.swift
+++ War/ViewController.swift
@@ -9,7 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//created a proprty to the viewcontroller class
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftPlayerScore: UILabel!
+    
+    @IBOutlet weak var rightPlayerScore: UILabel!
+    //touch up inside is when the user just taps the button
+    @IBAction func dealTapped(_ sender: Any) {
+        print("deal")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
