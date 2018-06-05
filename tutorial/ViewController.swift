644449f594e9b684bd693e66cb6d492ea848c2d1
javimgit1224 <36682043+javimgit1224@users.noreply.github.com>
2018-06-05 12:48:16 -0700

    Add files via upload
    finished UI for the app

---
+++ tutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Javier Melendrez on 6/5/18.
+//  Copyright © 2018 Javier Melendrez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
