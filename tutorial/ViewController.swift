d4daccf3b74044109c02d67fed11b283051e3872
javimgit1224 <36682043+javimgit1224@users.noreply.github.com>
2018-06-05 13:48:55 -0700

    Add files via upload
    connected ui to code

--- tutorial/ViewController.swift
+++ tutorial/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    //mark: properties
+    @IBOutlet weak var EnterMealName: UITextField!
+    @IBOutlet weak var ChangeMealName: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        EnterMealName.delegate = self
     }
-
+    
+    //mark: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        EnterMealName.text = textField.text
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //mark: actions
+    @IBAction func UIButton(_ sender: Any) {
+        ChangeMealName.text = "Default Text"
+    }
 }
 
